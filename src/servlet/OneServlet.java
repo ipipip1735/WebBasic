dd79ca2454c89b7dfe3658e87c3cf1908bf7d427
ipipip1735 <toyota0626>
2020-09-10 08:16:32 +0800

    Update Servlet add TwoServlet.init()

--- src/servlet/OneServlet.java
+++ src/servlet/OneServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 /**
  * Created by Administrator on 2019/8/29.
@@ -15,15 +16,17 @@ import java.io.IOException;
 public class OneServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
-        System.out.println("~~  " + getClass().getSimpleName() + ".init  ~~");
+        System.out.println("~~  " + getClass().getSimpleName() + ".init2  ~~");
         System.out.println("config is " + config);
         super.init(config);
+        System.out.println("2222222222");
     }
 
     @Override
     public void init() throws ServletException {
-        System.out.println("~~  " + getClass().getSimpleName() + ".init  ~~");
+        System.out.println("~~  " + getClass().getSimpleName() + ".init1  ~~");
         super.init();
+        System.out.println("1111111");
     }
 
     public OneServlet() {
@@ -47,5 +50,22 @@ public class OneServlet extends HttpServlet {
             response.getWriter().println(request.getSession().getAttribute("name"));
             System.out.println("session|name = " + request.getSession().getAttribute("name"));
         }
+
+
+
+
+
+        //获取初始化参数
+        Enumeration<String> e = getInitParameterNames();//获取初始化参数
+        while (e.hasMoreElements()) {
+            System.out.println(e.nextElement());
+        }
+
+        System.out.println("----------");
+
+        getServletContext()//获取应用上下文
+                .getInitParameterNames()//获取初始化参数
+                .asIterator()
+                .forEachRemaining(System.out::println);
     }
 }
--- src/servlet/TwoServlet.java
+++ src/servlet/TwoServlet.java
@@ -1,20 +1,48 @@
 package servlet;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Enumeration;
 
 /**
  * Created by Administrator on 2019/8/31 8:50.
  */
 public class TwoServlet extends HttpServlet {
+
+    String one, appOne;
+
     public TwoServlet() {
         super();
         System.out.println("*********  " + getClass().getSimpleName() + ".Constructor  *********");
         System.out.println(this);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("~~~~  " + getClass().getSimpleName() + ".init2  ~~~~");
+        super.init(config);
+
+
+        Enumeration<String> e = config.getInitParameterNames();//获取初始化参数
+        while (e.hasMoreElements()) {
+            System.out.println(e.nextElement());
+        }
+        one = config.getInitParameter("one");
+
+        System.out.println("----------");
+
+
+        config.getServletContext()//获取应用上下文
+                .getInitParameterNames()//获取初始化参数
+                .asIterator()
+                .forEachRemaining(System.out::println);
+        appOne = config.getServletContext().getInitParameter("appOne");
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -23,5 +51,10 @@ public class TwoServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("~~" + getClass().getSimpleName() + ".doGet~~");
+
+        System.out.println("one is " + one);
+        System.out.println("appOne is " + appOne);
+
+
     }
 }
